d1cf32625435000cf07a7f0a2a948a60be842421
dhruz0y <98645495+dhruz0y@users.noreply.github.com>
2024-01-02 17:41:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -72,6 +72,14 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
+$("h1").click(function(){
+  if (!started) {
+ $("#level-title").text("Level " + level);
+ nextSequence();
+ started = true;
+}   
+})
+
 $(document).keypress(function() {
   if (!started) {
     $("#level-title").text("Level " + level);
